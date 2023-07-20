595b665edbe5b19b2378cb038fce99ed8296975c
Mahmoud-Elhoseny <elhoseny916@gmail.com>
2023-07-20 17:55:14 +0300

    ..

--- src/App.js
+++ src/App.js
@@ -5,17 +5,18 @@ import Section3 from './components/Section3';
 import Section4 from './components/Section4';
 import Subscribe from './components/Subscribe';
 import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <>
+    <Container>
       <Navbar />
       <Landing />
       <Section3 />
       <Section4 />
       <Subscribe />
       <Footer />
-    </>
+    </Container>
   );
 }
 
